70b29b536a4d13e53403c38312fc03431da32697
majdbrt <majdbrt@gmail.com>
2021-05-27 10:56:13 -0500

    update GUI.py - mp4 download functionality fully implemented

--- GUI.py
+++ GUI.py
@@ -1,6 +1,5 @@
-import sys
-
-from PyQt5.QtWidgets import QApplication
+from pytube import YouTube
+from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtWidgets import QVBoxLayout
@@ -8,14 +7,26 @@ from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QFormLayout
 from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QStatusBar
+from PyQt5.QtWidgets import QRadioButton
+from PyQt5.QtWidgets import QLabel
 
-class WidgetWindow(QWidget):
-    def __init__(self):
+class MainWindow(QMainWindow):
+    def __init__(self, video):
         super().__init__()
+        self.streamInfo = video
+        self.main_widget = QWidget()
+        self.setCentralWidget(self.main_widget)
         self.general_layout = QVBoxLayout()
 
+        self.setWindowTitle('YouTube Downloader')
+
+        self.status = QStatusBar()
+
+        self.setStatusBar(self.status)
+
         # select folder/directory implementation
-        self.directoryText = None
+        self.error_msg_directory = 'Error! Download destination is not selected.'
         self.select_directory_layout = QHBoxLayout()
         self.select_directory_button = QPushButton('...')
         self.select_directory_box = QLineEdit()
@@ -28,41 +39,128 @@ class WidgetWindow(QWidget):
         self.select_directory_box.textChanged.connect(self.getDirectory)
 
         # url text box implementation
-        self.url = None
+        self.error_msg_url = 'Connot retrieve video'
         self.url_layout = QFormLayout()
         self.url_box = QLineEdit()
         self.url_layout.addRow("Enter YouTube's video url: ", self.url_box)
 
         self.general_layout.addLayout(self.url_layout)
 
-        self.url_box.textChanged.connect(self.getURL)
+        self.url_box.textChanged.connect(self.getYoutubeStreams)
 
-        self.setLayout(self.general_layout)
+        # Download button implementation
+        self.download_button = QPushButton('Download')
+        self.general_layout.addWidget(self.download_button)
+        self.download_button.clicked.connect(self.download_stream)
 
+        self.main_widget.setLayout(self.general_layout)
 
     def changeDirectory(self):
         folder = QFileDialog.getExistingDirectory(self, "Select Directory")
         self.select_directory_box.setText(folder)
+        self.status.clearMessage()
 
     def getDirectory(self):
-        self.directoryText = self.select_directory_box.text()
-        print(self.directoryText)
+        self.streamInfo.directory = self.select_directory_box.text()
 
-    def getURL(self):
-        self.url = self.url_box.text()
-        print(self.url)
+    def getYoutubeStreams(self, url):
+        self.status.clearMessage()
+        if self.url_box.text() != '':
+            try:
+                video = YouTube(url)
+                self.streamInfo.url = url
+                self.new_widget = Stream_Widget(video, self.streamInfo)
+                self.new_widget.show()
+
+            except:
+                self.status.showMessage(self.error_msg_url)
+                self.setStatusBar(self.status)
+
+        else:
+            self.status.clearMessage()
+
+    def download_stream(self):
+        self.status.clearMessage()
+        if self.streamInfo.isNone() == False:
+            stream = YouTube(self.streamInfo.url)
+            chosenStream = stream.streams.get_by_itag(self.streamInfo.itag)
+            try:
+                chosenStream.download(self.streamInfo.directory)
+                self.status.showMessage('Sucessfuly Downloaded!')
+            except:
+                self.status.showMessage(self.error_msg_directory)
+        else:
+            self.status.showMessage('Fill all the required information!')
 
 
+class Stream_Widget(QWidget):
+    def __init__(self, video, streamInfo):
+        super().__init__()
+        self.video = video
+        self.streamInfo = streamInfo
+        self.setWindowTitle('YouTube Downloader')
+        self.general_layout = QVBoxLayout()
+
+        self.select_format_layout = QHBoxLayout()
+
+        self.mp3_button = QRadioButton('mp3')
+        self.mp4_button = QRadioButton('mp4')
+
+        self.streams_dict = dict()
+
+        self.stream_selection_box = QLineEdit()
+        self.continue_button = QPushButton('Continue')
+
+        self.select_format_layout.addWidget(self.mp3_button)
+        self.select_format_layout.addWidget(self.mp4_button)
+
+        self.general_layout.addLayout(self.select_format_layout)
+        self.setLayout(self.general_layout)
+
+        self.mp4_button.toggled.connect(self.show_streams)
+        self.continue_button.clicked.connect(self.get_stream_type)
+
+    def get_stream(self):
+        streams = self.video.streams.filter(file_extension='mp4', progressive=True)
+
+        for stream in streams:
+            streamStr = str(stream).split()
+            itag = None
+            res = None
+            for word in streamStr:
+                if 'itag' in word:
+                    itag = word.strip('itag="')
+                elif 'res=' in word:
+                    res = word.strip('res="')
+            # for
+
+            self.streams_dict[itag] = res
+        # for
+
+    def show_streams(self):
+        self.streamInfo.type = 'mp4'
+        self.get_stream()
+        self.general_layout.addWidget(QLabel('Available streams:'))
+
+        for itag in self.streams_dict:
+            self.general_layout.addWidget(QLabel(self.streams_dict[itag]))
+        self.general_layout.addWidget(QLabel('Please enter the desired stream:'))
+        self.general_layout.addWidget(self.stream_selection_box)
+        self.general_layout.addWidget(self.continue_button)
+
+    def get_stream_type(self):
+        stream_exists = False
+        for itag in self.streams_dict:
+            if self.stream_selection_box.text() == self.streams_dict[itag]:
+                stream_exists = True
+
+        if stream_exists == False:
+            self.stream_selection_box.setText('Invalid stream!')
+        else:
+            self.streamInfo.quality = self.stream_selection_box.text()
+            for itag in self.streams_dict:
+                if self.streams_dict[itag] == self.streamInfo.quality:
+                    self.streamInfo.itag = itag
+            self.close()
 
 
-
-def main():
-    app = QApplication(sys.argv)
-    window = WidgetWindow()
-
-    window.show()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,40 +1,32 @@
-from pytube import YouTube
-# hello
-print('Welcome to this Youtube downloader!')
+import sys
+from PyQt5.QtWidgets import QApplication
+from GUI import MainWindow
 
-savePath = input("Enter the download's path: ")
-url = input("Enter the video's url: ")
-try:
-    video = YouTube(url)
-except:
-    print("Connot retrieve video")
+__author__ = 'Majd Barakat'
+__version__ = "1.0.1"
 
-def  getResolution():
-    quality = input("Enter video's resolution: ")
-    if quality == 'default' or quality == 'Default':
-        return  '360p'
-    elif quality == '144p' or quality == '240p' or quality == '360p' or quality == '480p':
-        return quality
-    else:
-        print('Enter a valid resolution')
-        getResolution()
+class Stream:
+    def __init__(self):
+        """Class to contain video information"""
+        self.url = None
+        self.type = None
+        self.quality = None
+        self.itag = None
+        self.directory = None
 
-quality = getResolution()
+    def isNone(self):
+        if self.url == None or self.type == None or self.quality == None or self.itag == None or self.directory == None:
+            return True
+        return False
 
-stream = video.streams.filter(file_extension='mp4',res= quality, progressive='True')
+def main():
+    app = QApplication(sys.argv)
+    video = Stream()
+    window = MainWindow(video)
 
-def getTag(stream):
-    [[key, value]] = stream.itag_index.items()
-    return key
-
-tag = getTag(stream)
+    window.show()
+    sys.exit(app.exec_())
 
 
-d_video = video.streams.get_by_itag(tag)
-try:
-    # downloading the video
-    d_video.download(savePath)
-except:
-    print("Some Error!")
-
-print('Video Downloaded Successfully!')
+if __name__ == '__main__':
+    main()
